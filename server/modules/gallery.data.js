d826f9a93c0447033a40a97ebb7c3f6b223edf42
Leila Sadr <leilasadr54@gmail.com>
2023-04-30 14:29:46 -0700

    finally resolved crashed server and updated gallery.data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/Viola_Davis.jpg', description: "My absolute favorite and most inspiring role of Viola Davis'!", likes: 0 },
+    {id: 2, path: 'images/Melissa_McCarthy.jpg', description: "She inspired me all the way in Iran to go to Groundlings!", likes: 0 },
+    {id: 3, path: 'images/Sandra_Bullock.jpg', description: "Her physical comedy is so precise and very funny!", likes: 0 },
+    {id: 4, path: 'images/Mehran_Modiri.jpg', description: "The one in the middle made Iran's first sitcom and why I wanted to do comedy!", likes: 0 },
+    {id: 5, path: 'images/Jim_Carrey.jpg', description: "I grew up with him, and he shapped my physical comedy skills!", likes: 0 },
+    {id: 6, path: 'images/Robin_Williams.jpg', description: "He is the most genius comic of my lifetime not to mention so heart warming and inspiring as a human being.", likes: 0 }
 ];
 
 module.exports = galleryItems;
