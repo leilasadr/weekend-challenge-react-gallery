26d5b13598221906061e72704a18734c36004823
Leila Sadr <leilasadr54@gmail.com>
2023-04-30 14:48:42 -0700

    componentized Header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,9 @@ import './App.css';
 function App() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
+       
+       <Header />
+        
         
       </div>
     );
